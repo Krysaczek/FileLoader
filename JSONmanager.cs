ddbe1825b61b1b8e931382c69f575626d82ebfdd
Krysaczek <krysaczek@gmail.com>
2020-01-06 00:13:53 +0100

    add basic logger

--- JSONmanager.cs
+++ JSONmanager.cs
@@ -10,15 +10,15 @@
 
     public static class JSONmanager
     {
-        static string _defaultPath = @"f:\RS\Exchange\jsonDefault\";
-        static string _defaultURL = "https://storage.googleapis.com/osb-exchange/summary.json";
+        private static string _defaultPath = @"f:\RS\Exchange\jsonDefault\";
+        private static string _defaultURL = "https://storage.googleapis.com/osb-exchange/summary.json";
         private static string _configPath = @"config/defaultPath.txt";
         private static string _configURL = @"config/jsonURL.txt";
 
         private static void LoadStrings()
         {
-            LoadTextFromFile(_defaultPath);
-            LoadURLFromFile(_defaultURL);
+            _defaultPath = LoadPathFromFile();
+            _defaultURL = LoadURLFromFile();
         }
 
         public static void SaveJson()
@@ -27,40 +27,39 @@
 
             using (var client = new WebClient())
             {
+                Logger.Log($"{GetCurrentTimePrefix()}: Saving"); // refactor with try catch
                 client.DownloadFile(_defaultURL, $@"{_defaultPath}{GetCurrentTimePrefix()}_summary.json");
             }
         }
 
-        private static void WriteTest()
-        {
-            string fileToCopy = @"f:\RS\Exchange\test.json";
-            File.Copy(fileToCopy, _defaultPath + Path.GetFileName(fileToCopy));
-        }
-
-        private static void LoadTextFromFile(string target)
+        public static string LoadPathFromFile()
         {
             if (File.Exists(_configPath))
             {
-                target = File.ReadAllText(_configPath);
+                var temp = File.ReadAllText(_configPath);
 
-                if (!string.IsNullOrWhiteSpace(target) && Directory.Exists(target))
+                if (!string.IsNullOrWhiteSpace(temp) && Directory.Exists(temp))
                 {
-                    _defaultPath = target;
+                    return temp;
                 }
             }
+
+            return _defaultPath;
         }
 
-        private static void LoadURLFromFile(string target)
+        private static string LoadURLFromFile()
         {
             if (File.Exists(_configURL))
             {
-                target = File.ReadAllText(_configURL);
+                var temp = File.ReadAllText(_configURL);
 
-                if (!string.IsNullOrWhiteSpace(target) && CheckURLValid(target))
+                if (!string.IsNullOrWhiteSpace(temp) && CheckURLValid(temp))
                 {
-                    _defaultURL = target;
+                    return temp;
                 }
             }
+
+            return _defaultURL;
         }
 
         private static string GetCurrentTimePrefix()
--- Logger.cs
+++ Logger.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FileLoader
 {
-    class Logger
+    public static class Logger
     {
+        static string _defaultPath = @"f:\RS\Exchange\jsonDefault\";
+        private static string _configPath = @"config/defaultPath.txt";
+
+        public static void Log(string message)
+        {
+            _defaultPath = JSONmanager.LoadPathFromFile();
+            _defaultPath = Path.GetFullPath(Path.Combine(_defaultPath, ".."));
+
+            var logFile = _defaultPath + @"\log.txt";
+            if (!File.Exists(logFile))
+            {
+                using (var file = File.Create(logFile))
+                {
+                }
+            }
+
+            File.AppendAllText(logFile, message + Environment.NewLine);
+        }
     }
 }
